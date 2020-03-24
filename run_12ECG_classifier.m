b2bfc4fbbcf7953c471a4954ae5afdf1f8b8a111
sebasmos <ulsordonez@unicauca.edu.co>
2020-03-24 17:46:50 -0500

    plotted 2

--- run_12ECG_classifier.m
+++ run_12ECG_classifier.m
@@ -15,12 +15,13 @@ function [score, label] = run_12ECG_classifier(data,header_data,classes, model,k
     % Use your classifier here to obtain a label and score for each class.
     features = get_12ECG_features(data,header_data);
     
-    
-     subplot(5,2,k);
-     for i = 1:12
-          plot(data(i,:)), hold on, grid on, xlabel('Frequency (Hz)'),
-          title(['Recording ',num2str(k)])
-     end
+    %% Signal plot
+%      subplot(5,2,k);
+%      for i = 1:12
+%           plot(data(i,:)), hold on, grid on, xlabel('Frequency (Hz)'),
+%           title(['Recording ',num2str(k)])
+%      end
+    %% end signal plot
     score = mnrval(model,features);		
     [~,idx] = max (score);
     label(idx)=1;
