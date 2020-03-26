81a38299b8a291c9494b1d0319c0a6a5a327b300
sebasmos <ulsordonez@unicauca.edu.co>
2020-03-26 02:14:42 -0500

    Se añade metodo de decomposicion empirico para denoising EMD

--- run_12ECG_classifier.m
+++ run_12ECG_classifier.m
@@ -11,9 +11,23 @@ function [score, label] = run_12ECG_classifier(data,header_data,classes, model,k
 %     FilteredData = filtering(data,0.005,55,500,1);
 %     FilteredData = FilteredData';
         for i =1:12
+                %% wavelets without thresholding
 %               [C,L] = wavedec(data(i,:),3,'sym6');  
 %               Constructed_Signal(i,:) = wrcoef('a',C,L,'sym6',3);
+                %% Denoising wavelet using thresholding and N decom/construnction levels
+                % [C,L] = wavedec(data(i,:),4,'db10'); 
+                % [thr,sorh,keepapp]=ddencmp('den','wv',data(i,:));
+                % A3=wdencmp('gbl',C,L,'db10',4,thr,sorh,keepapp);
+                % Constructed_Signal(i,:) = detrend(A3);
+                %% Denoising stage using Empirical Decomposition Method (EMD)
+%               cleanedSignal = emd_dfadenoising (data);
+%               Constructed_Signal(i,:) = cleanedSignal(i,:)';
+                %% Smoothing using Savitzky
                 Constructed_Signal(i,:) = sgolayfilt(data(i,:),3,41);
+                % Denoising stage using moving median smoothing filter
+%                 A3  = movmedian(Araw,11);
+%                 cleanedSignal = detrend(A3);%consider using detrending?
+                
         end
     %% Fourier Analysis 
     % Fourier_Analysis(data);
