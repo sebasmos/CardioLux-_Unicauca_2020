a4d52f76a5c5a443f527ab7a11eec06348e3944d
sebasmos <ulsordonez@unicauca.edu.co>
2020-04-03 21:14:07 -0500

    Se anade dx

--- get_12ECG_features.m
+++ get_12ECG_features.m
@@ -7,7 +7,7 @@ function features = get_12ECG_features(data, header_data,k)
 
 	% read number of leads, sample frequency and gain from the header.	
 
-	[recording,Total_time,num_leads,Fs,gain,age,sex]=extract_data_from_header(header_data);
+	[recording,Total_time,num_leads,Fs,gain,age,sex,dx]=extract_data_from_header(header_data);
 
 	HRVparams.Fs=Fs;
         HRVparams.PeakDetect.windows = floor(Total_time-1);
@@ -23,10 +23,10 @@ function features = get_12ECG_features(data, header_data,k)
                 % median filter to remove bw
                 for i=1:num_leads
                   
-                    %Constructed_Signal(i,:) = medianfilter(Lead12wGain(i,:)', Fs);
-                     Constructed_Signal(i,:) = Preprocessing(Lead12wGain(i,:),500,0.5,5);
+                    Constructed_Signal(i,:) = medianfilter(Lead12wGain(i,:)', Fs);
+%                      Constructed_Signal(i,:) = Preprocessing(Lead12wGain(i,:),500,0.5,5);
                                   
-                      % ECG12filt(i,:) = sgolayfilt(Lead12wGain(i,:)',3,25);
+%                       Constructed_Signal(i,:) = sgolayfilt(Lead12wGain(i,:)',3,25);
                 %ECG12filt(i,:) = medianfilter(Lead12wGain(i,:)', Fs);
 %                 % wavelets without thresholding
 %                 [C,L] = wavedec(Lead12wGain(i,:)',2,'db4');  
@@ -91,10 +91,11 @@ function features = get_12ECG_features(data, header_data,k)
                 features(25:108)= features1;
                 %12-leads/AF features
                 features(109:300) = features2;
+                features(301)=dx;
                
 
 	catch
-		features = NaN(1,24);
+		features = NaN(1,301);
 	end
 
 end
@@ -143,4 +144,65 @@ function ECGF = Preprocessing(ECG,fs,f1,f2)
     end_remove = total_zeros-start_remove;
     ECGF = ECGF(start_remove+1:end-end_remove);
 end
-
+% 
+% function features = get_12ECG_features(data, header_data)
+% 
+%        % addfunction path needed
+%         addpath(genpath('Tools/'))
+%         load('HRVparams_12ECG','HRVparams')
+% 
+% 	% read number of leads, sample frequency and gain from the header.	
+% 
+% 	[recording,Total_time,num_leads,Fs,gain,age,sex,dx]=extract_data_from_header(header_data);
+% 
+% 	HRVparams.Fs=Fs;
+%         HRVparams.PeakDetect.windows = floor(Total_time-1);
+%         HRVparams.windowlength = floor(Total_time);
+% 
+% 	try
+% 
+%                 for i =1:num_leads
+%                         Lead12wGain(i,:) = data(i,:)* gain(i);
+%                 end
+% 
+% 
+%                 % median filter to remove bw
+%                 for i=1:num_leads
+%                          ECG12filt(i,:) = medianfilter(Lead12wGain(i,:)', Fs);   
+% %                          [C,L] = wavedec(Lead12wGain(i,:)',2,'sym4');  
+% %                  % approx = appcoef(L,C,'sym4');
+% %                  % [cd1,cd2,cd3] = detcoef(C,L,[1 2 3]);
+% %                          ECG12filt1(i,:) = wrcoef('a',C,L,'sym4',2);
+%                 end
+% 
+%                 % convert 12Leads to XYZ leads using Kors transformation
+%                 XYZLeads = Kors_git(ECG12filt);
+% 
+%                 VecMag = vecnorm(XYZLeads');
+% 
+% 
+%                 % Convert ECG waveform in rr intervals
+%                 [t, rr, jqrs_ann, SQIvalue , tSQI] = ConvertRawDataToRRIntervals(VecMag, HRVparams, recording);
+%                 sqi = [tSQI', SQIvalue'];
+% 
+%                 % Find fiducial points using ECGKit
+%                 ECG_header.nsig = 1; ECG_header.freq = Fs; ECG_header.nsamp = length(VecMag);
+%                 wavedet_config.setup.wavedet.QRS_detection_only = 0;
+%                 [Fid_pts,~,~] = wavedet_3D_ECGKit(VecMag', jqrs_ann', ECG_header, wavedet_config);
+% 
+%                 [XYZ_Median,Fid_pts_Median] = Time_coherent_code_github(XYZLeads,Fid_pts,Fs);
+% 
+%                 GEH_features = GEH_analysis_git(XYZ_Median,Fid_pts_Median,Fs);
+% 
+%                 features(1)=age;
+%                 features(2)=sex;
+%                 features(3:24)=GEH_features;
+%                 features(25) = dx;
+% 
+% 
+% 	catch
+% 		features = NaN(1,25);
+% 	end
+% 
+% end
+% 
